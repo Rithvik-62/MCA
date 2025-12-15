3e6feed5a6367ab84eea3a9dbd04bb2e227d85b0
Rithvik <166587025+Rithvik-62@users.noreply.github.com>
2025-12-15 20:05:25 +0530

    Add files via upload

--- .NET/onesTwosComplement/Program.cs
+++ .NET/onesTwosComplement/Program.cs
@@ -1,92 +1,57 @@
 ﻿using System;
-namespace numconversion
+
+class OnesTwosComplement
 {
-    class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        int r, n, t, m, p, flag = 0;
+        int[] onesbit = new int[10];
+        int[] twosbit = new int[10];
+
+        Console.WriteLine("Enter a decimal Number : ");
+        n = int.Parse(Console.ReadLine());
+
+        p = 1;
+        m = 0;
+        int i = 0;
+
+        t = n;   // store original value
+
+        while (n != 0)
         {
-            int r, n, t, m, b, p;
+            r = n % 2;
+            m = m + r * p;
+            n = n / 2;
+            p = p * 10;
 
-            Console.Write("Enter a decimal number: ");
-            n = int.Parse(Console.ReadLine());
+            // 1's complement
+            onesbit[i] = (r == 0) ? 1 : 0;
 
-            while (true)
+            // 2's complement logic using flag
+            if (flag == 0)
             {
-                Console.Write("\nEnter 2/8/16 for base and 3 for exit: ");
-                b = int.Parse(Console.ReadLine());
-
-                switch (b)
-                {
-                    case 2:
-                    case 8:
-                        t = n;
-                        p = 1;
-                        m = 0;
-
-                        if (t == 0)
-                        {
-                            if (b == 2)
-                                Console.WriteLine("The binary equivalent = 0");
-                            else
-                                Console.WriteLine("The octal equivalent = 0");
-                            break;
-                        }
-
-                        while (t != 0)
-                        {
-                            r = t % b;
-                            m = m + r * p;
-                            t = t / b;
-                            p = p * 10;
-                        }
-
-                        if (b == 2)
-                            Console.WriteLine("The binary equivalent = " + m);
-                        else
-                            Console.WriteLine("The octal equivalent = " + m);
-                        break;
-
-                    case 16:
-                        t = n;
-                        int c = 0;
-                        char[] d = new char[32];
-
-                        if (t == 0)
-                        {
-                            Console.WriteLine("The hexadecimal equivalent = 0");
-                            break;
-                        }
-
-                        while (t != 0)
-                        {
-                            r = t % 16;
-
-                            if (r < 10)
-                                d[c] = Convert.ToChar(48 + r);   // '0' + r
-                            else
-                                d[c] = Convert.ToChar(55 + r);   // 'A' + (r - 10)
-
-                            t = t / 16;
-                            c++;
-                        }
-
-                        Console.Write("The hexadecimal equivalent = ");
-                        for (int i = c - 1; i >= 0; i--)
-                            Console.Write(d[i]);
-
-                        Console.WriteLine();
-                        break;
-
-                    case 3:
-                        Console.WriteLine("Exiting...");
-                        Environment.Exit(0);
-                        break;
-
-                    default:
-                        Console.WriteLine("Invalid choice..");
-                        break;
-                }
+                twosbit[i] = r;     // copy bits until first 1
+                if (r == 1)
+                    flag = 1;
             }
+            else
+            {
+                twosbit[i] = (r == 0) ? 1 : 0; // invert bits
+            }
+
+            i++;
         }
+
+        Console.WriteLine("\nBinary equivalent : " + m);
+
+        Console.Write("Ones complement : ");
+        for (int j = i - 1; j >= 0; j--)
+            Console.Write(onesbit[j]);
+        Console.WriteLine();
+
+        Console.Write("Twos complement : ");
+        for (int j = i - 1; j >= 0; j--)
+            Console.Write(twosbit[j]);
+        Console.WriteLine();
     }
 }
